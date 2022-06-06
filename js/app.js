fcd6239143e60ca4de63aac905bcae1a59305c1c
Kawsar Ahmed <kawsarahmed4148@gmail.com>
2022-06-07 01:52:52 +0600

    Pin Match Project Completed successfully

--- js/app.js
+++ js/app.js
@@ -16,3 +16,43 @@ document.getElementById('generate-pin').addEventListener('click', function(){
     document.getElementById('input-pin').value = pin;
 
 })
+
+
+
+document.getElementById('key-pad').addEventListener('click', function(event){
+    const number = event.target.innerText
+    const prevCalcInput = document.getElementById('calcInput')
+    if(isNaN(number)){
+        if(number == 'C'){
+            prevCalcInput.value = '';
+        }
+    }
+    else{
+        
+        const prevCalcInputNumber = prevCalcInput.value;
+        const newCalkInputNumber = prevCalcInputNumber + number;
+        prevCalcInput.value = newCalkInputNumber;
+    }
+
+})
+
+
+document.getElementById('submit').addEventListener('click', function(){
+    
+        const pin = document.getElementById('input-pin').value;
+        const typedNumbers = document.getElementById('calcInput').value;
+        const successMassage = document.getElementById('notify-success');
+        const failError = document.getElementById('notify-fail');
+        if (pin == typedNumbers) {
+            
+            successMassage.style.display = 'block';
+            failError.style.display = 'none';
+        }
+        else {
+            
+            failError.style.display = 'block';
+            successMassage.style.display = 'none';
+        }
+
+
+})
